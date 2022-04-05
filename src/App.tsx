198773e90c9a5fe8d5146e56cb02a146873d14df
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-04-05 14:26:38 +0700

    feat(blog): adding blog section

--- src/App.tsx
+++ src/App.tsx
@@ -343,6 +343,114 @@ const App = () => {
           </div>
         </div>
       </section>
+
+      {/* ? Blog */}
+      <section id="blog" className="pt-36 pb-32 bg-slate-100">
+        <div className="container">
+          <div className="w-full px-4">
+            <div className="max-w-xl mx-auto text-center mb-16">
+              <h4 className="font-semibold text-lg text-primary mb-2">Blog</h4>
+              <h2 className="font-bold text-dark text-3xl mb-4 md:text-4xl lg:text-5xl">
+                Tulisan Terkini
+              </h2>
+              <p className="font-medium text-base text-secondary md:text-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repellendus inventore sunt blanditiis voluptas?
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap">
+            <div className="w-full px-4 lg:w-1/2 xl:w-1/3 ">
+              <div className="bg-white rounded-xl overflow-hidden shadow-lg mb-10">
+                <img
+                  src="https://source.unsplash.com/360x200?programming"
+                  alt="Blog-1"
+                  className="w-full"
+                />
+                <div className="py-8 px-6">
+                  <h3>
+                    <a
+                      href="#"
+                      className="block mb-3 font-semibold text-xl text-dark hover:text-primary truncate"
+                    >
+                      Tips Belajar Programming
+                    </a>
+                  </h3>
+                  <p className="font-medium text-base text-secondary mb-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Culpa, saepe!
+                  </p>
+                  <a
+                    href="#"
+                    className="font-medium text-sm text-white bg-primary px-4 py-3 rounded-lg hover:opacity-80"
+                  >
+                    Baca Selengkapnya
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-4 lg:w-1/2 xl:w-1/3 ">
+              <div className="bg-white rounded-xl overflow-hidden shadow-lg mb-10">
+                <img
+                  src="https://source.unsplash.com/360x200?mechanical+keyboard"
+                  alt="Blog-1"
+                  className="w-full"
+                />
+                <div className="py-8 px-6">
+                  <h3>
+                    <a
+                      href="#"
+                      className="block mb-3 font-semibold text-xl text-dark hover:text-primary truncate"
+                    >
+                      Tips Membeli Keyboard Mechanical
+                    </a>
+                  </h3>
+                  <p className="font-medium text-base text-secondary mb-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Culpa, saepe!
+                  </p>
+                  <a
+                    href="#"
+                    className="font-medium text-sm text-white bg-primary px-4 py-3 rounded-lg hover:opacity-80"
+                  >
+                    Baca Selengkapnya
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-4 lg:w-1/2 xl:w-1/3 ">
+              <div className="bg-white rounded-xl overflow-hidden shadow-lg mb-10">
+                <img
+                  src="https://source.unsplash.com/360x200?macbook"
+                  alt="Blog-1"
+                  className="w-full"
+                />
+                <div className="py-8 px-6">
+                  <h3>
+                    <a
+                      href="#"
+                      className="block mb-3 font-semibold text-xl text-dark hover:text-primary truncate"
+                    >
+                      Tips Macbook dengan chipset Apple Silicon
+                    </a>
+                  </h3>
+                  <p className="font-medium text-base text-secondary mb-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Culpa, saepe!
+                  </p>
+                  <a
+                    href="#"
+                    className="font-medium text-sm text-white bg-primary px-4 py-3 rounded-lg hover:opacity-80"
+                  >
+                    Baca Selengkapnya
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
