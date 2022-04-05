a26309b1e788d4434c5d9b88aef27de692196138
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-04-05 20:53:40 +0700

    feat(contact): adding contact me section

--- src/App.tsx
+++ src/App.tsx
@@ -451,6 +451,73 @@ const App = () => {
           </div>
         </div>
       </section>
+
+      {/* Contact */}
+      <section id="contact" className="pt-36 pb-32">
+        <div className="container">
+          <div className="w-full px-4">
+            <div className="max-w-xl mx-auto text-center mb-16">
+              <h4 className="font-semibold text-lg text-primary mb-2">
+                Kontak
+              </h4>
+              <h2 className="font-bold text-dark text-3xl mb-4 md:text-4xl lg:text-5xl">
+                Hubungi Saya
+              </h2>
+              <p className="font-medium text-base text-secondary md:text-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repellendus inventore sunt blanditiis voluptas?
+              </p>
+            </div>
+          </div>
+          <form>
+            <div className="w-full lg:w-1/2 lg:mx-auto">
+              <div className="w-full px-4 mb-8">
+                <label
+                  htmlFor="name"
+                  className="text-base font-bold text-primary"
+                >
+                  Nama
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full bg-slate-100 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
+                />
+              </div>
+              <div className="w-full px-4 mb-8">
+                <label
+                  htmlFor="email"
+                  className="text-base font-bold text-primary"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="w-full bg-slate-100 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
+                />
+              </div>
+              <div className="w-full px-4 mb-8">
+                <label
+                  htmlFor="message"
+                  className="text-base font-bold text-primary"
+                >
+                  Pesan
+                </label>
+                <textarea
+                  id="message"
+                  className="w-full bg-slate-100 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary h-32"
+                ></textarea>
+              </div>
+              <div className="w-full px-4">
+                <button className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-300">
+                  Kirim
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
